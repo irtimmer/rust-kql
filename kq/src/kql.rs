dc99d63da0b0737e0be09e3ecdae759facb427d6
Iwan Timmer <irtimmer@gmail.com>
2023-12-25 18:43:45 +0100

    kq: initial KQL query tool for CSV files

---
+++ kq/src/kql.rs
@@ -0,0 +1,57 @@
+use datafusion::arrow::datatypes::DataType;
+use datafusion::catalog::ResolvedTableReference;
+use datafusion::catalog::TableReference;
+use datafusion::datasource::DefaultTableSource;
+use datafusion::error::{DataFusionError, Result as DFResult};
+use datafusion::execution::context::SessionState;
+use datafusion::execution::FunctionRegistry;
+use datafusion::logical_expr::{TableSource, AggregateUDF, ScalarUDF, LogicalPlan};
+
+use datafusion_kql::planner::{KqlToRel, ContextProvider};
+
+use kqlparser::parser::parse_tabular;
+
+use std::borrow::Cow;
+use std::collections::HashMap;
+use std::sync::Arc;
+
+struct SessionContextProvider<'a> {
+    state: &'a SessionState,
+    tables: HashMap<String, Arc<dyn TableSource>>,
+}
+
+pub async fn execute_kql(state: &SessionState, query: &str) -> Result<LogicalPlan, DataFusionError> {
+    let mut provider = SessionContextProvider {
+        state: &state,
+        tables: HashMap::with_capacity(10),
+    };
+
+    let catalog_list = state.catalog_list();
+    for catalog in catalog_list.catalog_names() {
+        let schema_list = catalog_list.catalog(&catalog).ok_or_else(|| DataFusionError::Plan(format!("Catalog '{catalog}' not found")))?;
+        for schema in schema_list.schema_names() {
+            let table_list = schema_list.schema(&schema).ok_or_else(|| DataFusionError::Plan(format!("Schema '{schema}' not found")))?;
+            for table in table_list.table_names() {
+                let resolved_ref = ResolvedTableReference {
+                    catalog: Cow::Borrowed(&catalog),
+                    schema: Cow::Borrowed(&schema),
+                    table: Cow::Borrowed(&table)
+                };
+                let table_provider = table_list.table(&table).await.ok_or_else(|| DataFusionError::Plan(format!("Table '{table}' not found")))?;
+                provider.tables.insert(resolved_ref.to_string(), Arc::new(DefaultTableSource::new(table_provider)));
+            }
+        }
+    }
+
+    let kql = KqlToRel::new(&provider);
+    let query = parse_tabular(query.as_bytes()).unwrap().1;
+    kql.query_to_plan(query)
+}
+
+impl<'a> ContextProvider for SessionContextProvider<'a> {
+    fn get_table_provider(&self, name: TableReference) -> DFResult<Arc<dyn TableSource>> {
+        let catalog = &self.state.config_options().catalog;
+        let name = name.resolve(&catalog.default_catalog, &catalog.default_schema).to_string();
+        self.tables.get(&name).cloned().ok_or_else(|| DataFusionError::Plan(format!("Table '{}' not found", name)))
+    }
+}
---
+++ kq/src/main.rs
@@ -0,0 +1,40 @@
+use clap::Parser;
+
+use datafusion::arrow::record_batch::RecordBatch;
+use datafusion::arrow::util::pretty;
+use datafusion::execution::context::SessionContext;
+use datafusion::execution::options::CsvReadOptions;
+
+use std::error::Error;
+use std::path::PathBuf;
+
+mod kql;
+
+use crate::kql::execute_kql;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    file: PathBuf,
+    query: String
+}
+
+async fn execute(ctx: &SessionContext, query: &str) -> Result<(), Box<dyn Error>> {
+    let state = ctx.state();
+    let plan = execute_kql(&state, query).await?;
+    let results: Vec<RecordBatch> = ctx.execute_logical_plan(plan).await?.collect().await?;
+    pretty::print_batches(&results)?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+
+    let ctx = SessionContext::new();
+    let base = args.file.file_stem().unwrap().to_str().unwrap();
+    ctx.register_csv(base, args.file.as_os_str().to_str().unwrap(), CsvReadOptions::default()).await?;
+    execute(&ctx, &args.query).await?;
+
+    Ok(())
+}
