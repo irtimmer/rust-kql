af3a550b8aa25b90201ebc9b204a4cb7904baece
Iwan Timmer <irtimmer@gmail.com>
2024-10-07 18:01:49 +0200

    common: update DataFusion to version 42

--- datafusion-kql/src/planner.rs
+++ datafusion-kql/src/planner.rs
@@ -1,26 +1,21 @@
-use arrow_schema::{DataType, TimeUnit};
+use arrow_schema::{DataType, Field, TimeUnit};
 
-use datafusion_common::{TableReference, JoinType, Column, DFSchema, DFField, ScalarValue};
+use datafusion_common::{TableReference, JoinType, Column, DFSchema, ScalarValue};
 use datafusion_common::{DataFusionError, Result};
 
-use datafusion_expr::{aggregate_function, AggregateFunction as BuiltinAggregateFunction, BuiltinScalarFunction, BuiltInWindowFunction, WindowFrame, window_function, Values};
-use datafusion_expr::expr::{AggregateFunction, Sort, ScalarFunction, WindowFunction};
+use datafusion_expr::Values;
+use datafusion_expr::expr::{AggregateFunction, ScalarFunction, WindowFunction};
 use datafusion_expr::expr_fn::col;
+use datafusion_expr::planner::ContextProvider;
 use datafusion_expr::logical_plan::{LogicalPlan, LogicalPlanBuilder};
-use datafusion_expr::{Expr, Literal, TableSource};
+use datafusion_expr::{Expr, Literal, SortExpr};
 
 use itertools::Itertools;
 
 use kqlparser::ast::{Expr as KqlExpr, Operator, TabularExpression, Literal as KqlLiteral, Source, Type};
 
 use std::collections::HashMap;
-use std::iter;
-use std::str::FromStr;
 use std::sync::Arc;
-
-pub trait ContextProvider {
-    fn get_table_provider(&self, name: TableReference) -> Result<Arc<dyn TableSource>>;
-}
 
 #[derive(Default, Clone)]
 struct PlannerContext {}
@@ -36,22 +31,15 @@ impl<'a, S: ContextProvider> KqlToRel<'a, S> {
 
     fn func_to_expr(&self, ctx: &mut PlannerContext, name: &str, args: &Vec<KqlExpr>) -> Result<Expr> {
         let args = args.iter().map(|a| self.ast_to_expr(ctx, a)).collect::<Result<Vec<Expr>>>()?;
-        Ok(match name {
-            "make_list" => aggr_func(aggregate_function::AggregateFunction::ArrayAgg, args),
-            "split" => scalar_func(BuiltinScalarFunction::StringToArray, args),
-            "next" => window_func(BuiltInWindowFunction::Lag, args),
-            "prev" => window_func(BuiltInWindowFunction::Lead, args),
-            "row_number" => window_func(BuiltInWindowFunction::RowNumber, args),
-            _ => if let Ok(f) = BuiltinScalarFunction::from_str(name) {
-                scalar_func(f, args)
-            } else if let Ok(f) = BuiltinAggregateFunction::from_str(name) {
-                aggr_func(f, args)
-            } else if let Ok(f) = BuiltInWindowFunction::from_str(name) {
-                window_func(f, args)
-            } else {
-                return Err(DataFusionError::NotImplemented("Function not implemented".to_string()));
-            }
-        })
+        if let Some(f) = self.ctx.get_function_meta(&name) {
+            Ok(Expr::ScalarFunction(ScalarFunction::new_udf(f, args)))
+        } else if let Some(f) = self.ctx.get_aggregate_meta(&name) {
+            Ok(Expr::AggregateFunction(AggregateFunction::new_udf(f, args, false, None, None, None)))
+        } else if let Some(f) = self.ctx.get_window_meta(&name) {
+            Ok(Expr::WindowFunction(WindowFunction::new(f, args)))
+        } else {
+            return Err(DataFusionError::NotImplemented("Function not implemented".to_string()));
+        }
     }
 
     fn ast_to_expr(&self, ctx: &mut PlannerContext, ast: &KqlExpr) -> Result<Expr> {
@@ -79,10 +67,10 @@ impl<'a, S: ContextProvider> KqlToRel<'a, S> {
     fn query_statement_to_plan(&self, ctx: &mut PlannerContext, query: &TabularExpression) -> Result<LogicalPlan> {
         let mut builder = match &query.source {
             Source::Datatable(s, d) => LogicalPlanBuilder::from(LogicalPlan::Values(Values {
-                schema: Arc::new(DFSchema::new_with_metadata(s.iter().map(|(n, t)| DFField::new::<TableReference>(None, n, type_to_datatype(t), true)).collect(), HashMap::default()).unwrap()),
+                schema: Arc::new(DFSchema::new_with_metadata(s.iter().map(|(n, t)| (None::<TableReference>, Arc::new(Field::new(n, type_to_datatype(t), true)))).collect(), HashMap::default()).unwrap()),
                 values: d.iter().chunks(s.len()).into_iter().map(|chunk| chunk.map(|r| self.ast_to_expr(ctx, r).unwrap()).collect()).collect()
             })),
-            Source::Reference(n) => LogicalPlanBuilder::scan(n.clone(), self.ctx.get_table_provider(TableReference::from(n.as_str()))?, None)?,
+            Source::Reference(n) => LogicalPlanBuilder::scan(n.clone(), self.ctx.get_table_source(TableReference::from(n.as_str()))?, None)?,
             _ => return Err(DataFusionError::NotImplemented("Source not implemented".to_string())),
         };
 
@@ -91,7 +79,7 @@ impl<'a, S: ContextProvider> KqlToRel<'a, S> {
                 Operator::MvExpand(x) => builder.unnest_column(Column::from(x))?,
                 Operator::Extend(x) => {
                     let current_schema = builder.schema().clone();
-                    let current_columns = current_schema.fields().iter().map(|f| Expr::Column(f.qualified_column()));
+                    let current_columns = current_schema.columns().into_iter().map(|f| Expr::Column(f));
                     builder.project(current_columns.chain(x.iter().map(|(a, b)| {
                         let mut expr = self.ast_to_expr(ctx, b).unwrap();
                         if let Some(alias) = a {
@@ -123,7 +111,7 @@ impl<'a, S: ContextProvider> KqlToRel<'a, S> {
                     let mut ctx1 = ctx.clone();
                     builder.aggregate(y.iter().map(|z| self.ast_to_expr(&mut ctx1, z).unwrap()), x.iter().map(|z| self.ast_to_expr(ctx, z).unwrap()))?
                 },
-                Operator::Sort(o) => o.iter().fold(builder, |b, c| b.sort(iter::once(Expr::Sort(Sort::new(Box::new(col(c)), false, false)))).unwrap()),
+                Operator::Sort(o) => builder.sort(o.iter().map(|c| SortExpr::new(col(c), false, false)))?,
                 Operator::Take(x) => builder.limit(0, Some((*x).try_into().unwrap()))?,
                 _ => return Err(DataFusionError::NotImplemented("Operator not implemented".to_string())),
             };
@@ -161,16 +149,4 @@ fn literal_to_expr(val: &KqlLiteral) -> Expr {
         KqlLiteral::Timespan(x) => ScalarValue::DurationNanosecond(*x).lit(),
         _ => panic!("Not supported")
     }
-}
-
-fn aggr_func(func: aggregate_function::AggregateFunction, args: Vec<Expr>) -> Expr {
-    Expr::AggregateFunction(AggregateFunction::new(func, args, false, None, None))
-}
-
-fn scalar_func(func: BuiltinScalarFunction, args: Vec<Expr>) -> Expr {
-    Expr::ScalarFunction(ScalarFunction::new(func, args))
-}
-
-fn window_func(func: BuiltInWindowFunction, args: Vec<Expr>) -> Expr {
-    Expr::WindowFunction(WindowFunction::new(window_function::WindowFunction::BuiltInWindowFunction(func), args, vec![], vec![], WindowFrame::new(false)))
 }
--- kq/src/kql.rs
+++ kq/src/kql.rs
@@ -1,18 +1,20 @@
 use datafusion::arrow::datatypes::DataType;
-use datafusion::catalog::ResolvedTableReference;
-use datafusion::catalog::TableReference;
+use datafusion::catalog_common::ResolvedTableReference;
+use datafusion::catalog_common::TableReference;
+use datafusion::config::ConfigOptions;
 use datafusion::datasource::DefaultTableSource;
 use datafusion::error::{DataFusionError, Result as DFResult};
 use datafusion::execution::context::SessionState;
 use datafusion::execution::FunctionRegistry;
+use datafusion::logical_expr::WindowUDF;
 use datafusion::logical_expr::{TableSource, AggregateUDF, ScalarUDF, LogicalPlan};
+use datafusion::logical_expr::planner::ContextProvider;
 
-use datafusion_kql::planner::{KqlToRel, ContextProvider};
+use datafusion_kql::planner::KqlToRel;
 
 use kqlparser::ast::Statement;
 use kqlparser::parser::parse;
 
-use std::borrow::Cow;
 use std::collections::HashMap;
 use std::sync::Arc;
 
@@ -34,11 +36,11 @@ pub async fn execute_kql(state: &SessionState, query: &str) -> Result<LogicalPla
             let table_list = schema_list.schema(&schema).ok_or_else(|| DataFusionError::Plan(format!("Schema '{schema}' not found")))?;
             for table in table_list.table_names() {
                 let resolved_ref = ResolvedTableReference {
-                    catalog: Cow::Borrowed(&catalog),
-                    schema: Cow::Borrowed(&schema),
-                    table: Cow::Borrowed(&table)
+                    catalog: Arc::from(Box::from(catalog.clone())),
+                    schema: Arc::from(Box::from(schema.clone())),
+                    table: Arc::from(Box::from(table.clone()))
                 };
-                let table_provider = table_list.table(&table).await.ok_or_else(|| DataFusionError::Plan(format!("Table '{table}' not found")))?;
+                let table_provider = table_list.table(&table).await?.ok_or_else(|| DataFusionError::Plan(format!("Table '{table}' not found")))?;
                 provider.tables.insert(resolved_ref.to_string(), Arc::new(DefaultTableSource::new(table_provider)));
             }
         }
@@ -59,9 +61,41 @@ pub async fn execute_kql(state: &SessionState, query: &str) -> Result<LogicalPla
 }
 
 impl<'a> ContextProvider for SessionContextProvider<'a> {
-    fn get_table_provider(&self, name: TableReference) -> DFResult<Arc<dyn TableSource>> {
+    fn get_table_source(&self, name: TableReference) -> DFResult<Arc<dyn TableSource>> {
         let catalog = &self.state.config_options().catalog;
         let name = name.resolve(&catalog.default_catalog, &catalog.default_schema).to_string();
         self.tables.get(&name).cloned().ok_or_else(|| DataFusionError::Plan(format!("Table '{}' not found", name)))
+    }
+
+    fn get_function_meta(&self, name: &str) -> Option<Arc<ScalarUDF>> {
+        self.state.udf(name).ok()
+    }
+
+    fn get_aggregate_meta(&self, name: &str) -> Option<Arc<AggregateUDF>> {
+        self.state.udaf(name).ok()
+    }
+
+    fn get_variable_type(&self, _variable_names: &[String]) -> Option<DataType> {
+        None
+    }
+    
+    fn get_window_meta(&self, name: &str) -> Option<Arc<WindowUDF>> {
+        self.state.udwf(name).ok()
+    }
+    
+    fn options(&self) -> &ConfigOptions {
+        !unimplemented!()
+    }
+    
+    fn udf_names(&self) -> Vec<String> {
+        self.state.scalar_functions().keys().cloned().collect()
+    }
+    
+    fn udaf_names(&self) -> Vec<String> {
+        self.state.aggregate_functions().keys().cloned().collect()
+    }
+    
+    fn udwf_names(&self) -> Vec<String> {
+        self.state.window_functions().keys().cloned().collect()
     }
 }
